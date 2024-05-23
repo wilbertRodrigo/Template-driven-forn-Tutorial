22a5bec2c0807463c2ef04176c472d3b68a0d3a9
wilbertRodrigo <wilbert.rodrigo@pulsar-sd.com>
2024-05-23 14:37:03 +0800

    modification

---
+++ src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+// import { Hero } from '../hero';
+import { Login } from '../hero';
+import * as CryptoJS from 'crypto-js';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css'],
+})
+export class HeroFormComponent {
+  username: string = '';
+  password: string = '';
+
+  dept = ['Accounting', 'Sales', 'Marketing', 'Production', 'Quality', 'IT'];
+
+  model = new Login(18, this.username, this.password, this.dept[0]);
+
+  newLogin() {
+    this.model = new Login(42, this.username, this.password, this.dept[0]);
+    this.showDept = true;
+  }
+  submitted = false;
+  created = false;
+  showDept: boolean = false;
+  displayNotification: boolean = false;
+  showProceed = false;
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  onCreated() {
+    this.created = true;
+  }
+
+  creationSuccess() {
+    this.displayNotification = true;
+  }
+  constructor(private location: Location) {}
+  goBack(): void {
+    this.location.back();
+  }
+  closeBtn() {
+    window.location.reload();
+  }
+
+  proceed() {
+    alert('Account successfully created!');
+  }
+}
---
+++ src/app/hero.ts
@@ -0,0 +1,17 @@
+// export class Hero {
+//   constructor(
+//     public id: number,
+//     public name: string,
+//     public power: string,
+//     public alterEgo?: string
+//   ) {}
+// }
+
+export class Login {
+  constructor(
+    public id: number,
+    public username: string,
+    public password: string,
+    public dept?: string
+  ) {}
+}
